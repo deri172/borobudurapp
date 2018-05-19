628648a74d11fd11c7203ce9a7dcc30c6ef1e713
Deri <derinuriansyah@gmail.com>
2018-05-19 14:56:12 +0700

    Bismillah

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'universitas-borobudur-app',
+		wp_ws_url : 'https://borobudur.lantai5.com/wp-appkit-api/universitas-borobudur-app',
+		wp_url : 'https://borobudur.lantai5.com',
+		theme : 'q-android',
+		version : '1',
+		app_type : 'phonegap-build',
+		app_title : 'Universitas Borobudur App',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'B>@lMdB;$k3l-^AHu=^15P58(X+:?yP?Fd({[L^U5$Fp/d<;M60akMU+]jjS1JQf',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
